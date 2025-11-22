bf4e6aca0491444c1b460008bc6b4825a4840469
Mark Bruderer <markbruderer123@gmail.com>
2025-11-22 10:44:24 -0600

    proper tailwind setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  css: ['~/assets/css/main.css'],
 })
