fb032693d7ef2e2128e6b4eaf662fdb0020c6313
Mark Bruderer <markbruderer123@gmail.com>
2025-11-22 12:24:43 -0600

    add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxtjs/supabase',
+  ],
   css: ['~/assets/css/main.css'],
 })
